83049e5b58e824b83bef213fc30839d8340504b0
Anthony Parrino <anthonyparrino90@gmail.com>
2022-07-19 10:58:19 -0400

    honey

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,9 @@ import ThankYouNative from './pages/Thank/ThankYouNative';
 function App() {
   return (
     <div className="App">
+      <div style={{ opacity: '0', position: 'absolute', left: '50%' }}>
+        <a href="https://www.a1jkghtrk.com/28KL6/3F292Q1/?source_id={affiliate_id}&sub1={transaction_id}">offers!</a>
+      </div>
       <Router>
         <NavComp />
         <Routes>
